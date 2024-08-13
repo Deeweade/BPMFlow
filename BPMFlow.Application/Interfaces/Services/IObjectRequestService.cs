4f22f6185cdae721e6a7dfbceede198200a6bf7c
Maksimova, Lyubov <lmaksimova@rencredit.ru>
2024-08-13 12:40:18 +0300

    fix

--- BPMFlow.Application/Interfaces/Services/IObjectRequestService.cs
+++ BPMFlow.Application/Interfaces/Services/IObjectRequestService.cs
@@ -11,12 +11,18 @@ public interface IObjectRequestService
     
     /// <summary>
     /// Метод Create() создает запись в таблице ObjectRequest и возвращает эту запись на фронт
+    /// </summary>
+    /// <param name = "authorLogin"> Логин сотрудника, который создал запрос </param>
+    /// <summary>
+    Task<ObjectRequestView> Create(ObjectRequestView objectRequestView, string authorLogin);
+
+    /// <summary>
     /// Метод BulkCreate() создает одну и ту же запись в таблице ObjectRequest, но с разными ResponsibleEmployeeId,
         ///и возвращает на фронт массив кодов созданных записей
     /// </summary>
-    /// <param name="authorLogin"> Логин сотрудника, который создал запрос </param>
+    /// <param name = "employeeIds"> Id сотрудников, для которых назначается запрос </param> 
+    /// <param name = "authorLogin"> Логин сотрудника, который создал запрос </param>
     /// <summary>
-    Task<ObjectRequestView> Create(ObjectRequestView objectRequestView, string authorLogin);
     Task<IEnumerable<int>> BulkCreate(ICollection<int> employeeIds, ObjectRequestView objectRequests, string authorLogin);
 
     Task<IEnumerable<ObjectRequestView>> GetByFilter(ObjectRequestsFilterView filterView);
